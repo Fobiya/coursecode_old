755d054104f56c24d9588a422c6a45908713e362
Fobiya <stefanchuk.roman@gmail.com>
2020-10-31 15:58:57 +0200

    fix

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -8,7 +8,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: baseWebpackConfig.externals.paths.dist,
-    port: 8134,
+    port: 8077,
     overlay: {
       warnings: true,
       errors: true
